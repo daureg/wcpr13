668adc293894856f60b66e7b2bbcd80e4b23787f
Géraud Le Falher <daureg@gmail.com>
2013-05-24 22:40:31 +0200

    I mean, not just changing the filename.

--- build_POS_features.py
+++ build_POS_features.py
@@ -2,24 +2,33 @@
 # vim: set fileencoding=utf-8
 from nltk.tag import pos_tag
 from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.stem import WordNetLemmatizer
 from collections import defaultdict
+from scipy.io import savemat
+import numpy as np
 with open('small.corpus') as f:
-    first = f.readlines()[0].strip()
+    essays = f.readlines()[0:2]
 
-pos = []
-posd = defaultdict(int)
-wnl = WordNetLemmatizer()
 
-print(len(first))
-for s in sent_tokenize(first):
-    print(len(s))
-    wk = word_tokenize(s)
-    pos += pos_tag(wk)
-    for w in wk:
-        print(wnl.lemmatize(w))
+def get_pos_dict(text):
+    pos = []
+    posd = defaultdict(int)
+    for s in sent_tokenize(text):
+        wk = word_tokenize(s)
+        pos += pos_tag(wk)
 
-for p in pos:
-    posd[p[1]] += 1
+    for p in pos:
+        posd[p[1]] += 1
+    return posd
 
-print(posd)
+grammar_tag = set([])
+all_pos_tag = map(essays, get_pos_dict)
+for d in all_pos_tag:
+    grammar_tag |= d.keys()
+
+N = len(essays)
+POSmat = np.zeros((N, len(grammar_tag)), np.int16)
+for j, tag in enumerate(grammar_tag):
+    for i, d in enumerate(all_pos_tag):
+        POSmat[i, j] = d[tag]
+
+savemat('POS', {'POS': POSmat}, do_compression=True, oned_as='row')
