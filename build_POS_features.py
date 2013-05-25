c87665f1edb2106b5d53023459250a5459a82773
Géraud Le Falher <daureg@gmail.com>
2013-05-25 17:25:00 +0200

    use simpler tag and making it actually work

--- build_POS_features.py
+++ build_POS_features.py
@@ -2,6 +2,7 @@
 # vim: set fileencoding=utf-8
 from nltk.tag import pos_tag
 from nltk.tokenize import word_tokenize, sent_tokenize
+from nltk.tag.simplify import simplify_wsj_tag
 from collections import defaultdict
 from scipy.io import savemat
 import numpy as np
@@ -12,23 +13,39 @@ with open('small.corpus') as f:
 def get_pos_dict(text):
     pos = []
     posd = defaultdict(int)
+    # seems slower, despite the promising name
+    # pos = batch_pos_tag(text)
+    # pos = [tag for sent in batch_pos_tag(map(word_tokenize,
+    #                                          sent_tokenize(text.strip())))
+    #        for tag in sent]
     for s in sent_tokenize(text):
         wk = word_tokenize(s)
         pos += pos_tag(wk)
 
     for p in pos:
-        posd[p[1]] += 1
+        posd[simplify_wsj_tag(p[1])] += 1
     return posd
 
-grammar_tag = set([])
-all_pos_tag = map(essays, get_pos_dict)
-for d in all_pos_tag:
-    grammar_tag |= d.keys()
+if __name__ == '__main__':
+    # import timeit
+    # nrun = 3
+    # t = timeit.Timer('get_pos_dict(essays[0])',
+    #                  setup="from __main__ import get_pos_dict, essays")
+    # print(t.repeat(number=nrun))
+    grammar_tag = set([])
+    all_pos_tag = map(get_pos_dict, essays)
+    for d in all_pos_tag:
+        grammar_tag |= set(d.keys())
 
-N = len(essays)
-POSmat = np.zeros((N, len(grammar_tag)), np.int16)
-for j, tag in enumerate(grammar_tag):
-    for i, d in enumerate(all_pos_tag):
-        POSmat[i, j] = d[tag]
+    grammar_tag = list(grammar_tag)
+    with open('pos_tag_list', 'w') as f:
+        f.write('\n'.join(grammar_tag))
 
-savemat('POS', {'POS': POSmat}, do_compression=True, oned_as='row')
+    N = len(essays)
+    N = 2
+    POSmat = np.zeros((N, len(grammar_tag)), np.int16)
+    for j, tag in enumerate(grammar_tag):
+        for i, d in enumerate(all_pos_tag):
+            POSmat[i, j] = d[tag]
+
+    savemat('POS', {'POS': POSmat}, do_compression=True, oned_as='row')
