4a105b753617965188a4291484e600bc48b91e3f
Geraud Le Falher <daureg@gmail.com>
2013-05-27 21:35:21 +0300

    add a function to tf-idf weight the document matrix.

---
+++ ML/tfidf.m
@@ -0,0 +1,9 @@
+function W = tfidf(A)
+	[N, M] = size(A);
+	df = sum(A>0);
+	d = log((N)*ones(1, M)./df);
+	W = (A>0).*(1+log(A+1e-7)).*repmat(d, N, 1);
+	tic
+	bsxfun(@rdivide, W, sqrt(sum((W+1e-5).^2,1)))
+	toc
+end
